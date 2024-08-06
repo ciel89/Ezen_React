0cade6af96f41cd234fdaecdbf585d550e0c3fec
ciel89 <155046322+ciel89@users.noreply.github.com>
2024-08-06 18:41:36 +0900

    Add files via upload

---
+++ src - svg/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import { ReactComponent as Icon } from './icon.svg';
+
+const App = () => (
+  <div className="App">
+    <Icon className="rotating-icon" />
+  </div>
+);
+
+export default App;
+
